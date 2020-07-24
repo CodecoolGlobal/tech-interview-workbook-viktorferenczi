a65550c9db3ef47f285a32c8964a4a415df5b8ee
Szymon Seliga <SzymonSel@users.noreply.github.com>
2020-07-24 09:33:37 +0200

    Initialize bugfix/web

---
+++ _common/init.js
@@ -0,0 +1,14 @@
+window.$docsify = {
+  name: '',
+  repo: '',
+  loadSidebar: "sidebar.md",
+  maxLevel: 5,
+  subMaxLevel: 5,
+  auto2top: true,
+  executeScript: true,
+  homepage: 'README.md',
+  plugins: [],
+  requestHeaders: {
+    'Cache-Control': 'no-cache'
+  }
+};
